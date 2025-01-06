8662968a2ba4f45eb56b541b83b5d0990d5bd44b
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2025-01-06 21:42:23 +0600

    fixed hydration bug and setup redux

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,11 @@
 import { axiosBaseQuery } from "@/src/helpers/axios/axiosBaseQuery";
 import { createApi } from "@reduxjs/toolkit/query/react";
+import { tagTypesList } from "../tag-types";
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: axiosBaseQuery({ baseUrl: "https://spare-room-backend.vercel.app/api/v1" }),
   endpoints: () => ({}),
-  tagTypes: ['expense'],
+  tagTypes: tagTypesList,
 });
---
+++ src/redux/tag-types.ts
@@ -0,0 +1,11 @@
+export enum tagTypes {
+   expense = 'expense',
+   user = 'user',
+ 
+}
+
+export const tagTypesList = [
+   tagTypes.user,
+   tagTypes.expense,
+   
+];
